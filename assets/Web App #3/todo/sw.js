a5d8c8160d8b1a380ddd40d6e66fd043bedbe5c9
Max <shpax1@gmail.com>
2018-09-04 16:20:54 +0300

    sw.js upd

--- assets/Web App #3/todo/sw.js
+++ assets/Web App #3/todo/sw.js
@@ -1,25 +1,23 @@
+/* eslint-disable no-undef, no-restricted-globals */
 const CACHE_NAME = 'cart-app-cache';
 
 const assetsHandler = (event) => {
-  const isHtml = event.request.url.match(/\//);
+  const isHtml = event.request.url.match(/\/$/);
+  const isStatic = event.request.url.match(/\.(woff2|css|js)/);
 
-  if (!event.request.url.match(/\.(woff2|css|js)/) && !isHtml) return;
+  const shouldCacheRequest = isStatic || isHtml;
+
+  if (!shouldCacheRequest) return;
 
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
+      .then((cachedResponse) => {
         // Cache hit - return response
-        if (response && !isHtml) {
-          return response;
+        if (cachedResponse && !isHtml) {
+          return cachedResponse;
         }
 
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        const fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
+        return fetch(event.request).then(
           (response) => {
             // Check if we received a valid response
             if (!response || response.status !== 200 || response.type !== 'basic') {
@@ -30,18 +28,19 @@ const assetsHandler = (event) => {
             // and because we want the browser to consume the response
             // as well as the cache consuming the response, we need
             // to clone it so we have two streams.
-            const responseToCache = response.clone();
+            // const responseToCache = response.clone();
 
+            const resClone = response.clone();
             caches.open(CACHE_NAME)
               .then((cache) => {
-                cache.put(event.request, responseToCache);
+                cache.put(event.request, resClone);
               });
 
             return response;
           },
-        ).catch(err => isHtml && response);
+        ).catch(() => isHtml && cachedResponse);
       }),
   );
 };
 
-self.addEventListener('fetch', event => assetsHandler(event));
+self.addEventListener('fetch', assetsHandler);
